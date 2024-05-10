ee11cedbeb5e84debbd5110e4fc3c51cc6323c1a
Esther J. Iyege <faithesther20@gmail.com>
2024-05-10 02:42:54 +0100

    done done done dione :)

--- server/api/currency/[code].js
+++ server/api/currency/[code].js
@@ -1,17 +1,11 @@
-import { defineEventHandler } from 'h3';
-import { useRuntimeConfig } from '@nuxtjs/config-runtime'; // Import useRuntimeConfig
-import { useFetch } from 'nuxt'; // Import useFetch from 'nuxt' instead of 'nuxt/app'
-
 export default defineEventHandler(async (event) => {
-    const { code } = event.context.params;
-    const { currencyKey } = useRuntimeConfig();
-    const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&apikey=${currencyKey}`;
 
-    try {
-        const { data } = await useFetch(uri);
-        return data;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-    }
-});
+    const { code } = event.context.params
+    const { currencyKey } = useRuntimeConfig()
+  
+    const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&apikey=${currencyKey}`
+    
+    const { data } = await $fetch(uri)
+  
+    return data
+  })
